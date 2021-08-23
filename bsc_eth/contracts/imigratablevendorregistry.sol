f4b2079a1e58ff9212ae98f901ec0637981fdae6
loma <loma@sqdmc.net>
2021-08-23 06:28:23 -0700

    Add natspec comments to WrappedAsset, VendorRegistry, IMigratableWrappedAsset & IMigratableVendorRegistry.

--- bsc_eth/contracts/imigratablevendorregistry.sol
+++ bsc_eth/contracts/imigratablevendorregistry.sol
@@ -2,7 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+/// A collection of all methods on a previous VendorRegistry called by the new version of WrappedAsset during migration.
+/// The migration process does not use any VendorRegistry methods on the previous version that are not in this interface.
 interface IMigratableVendorRegistry
     {
+    /// Look up the MRX addrees for a given (ethereum or BSC) vendor address.
+    /// @param  vendorAddress The vendor address address for which to look up the MRX address.
+    /// @return The MRX address, or address(0) if the vendor address is not registered.
     function findMrxFromVendor(address vendorAddress) external view returns (address);
     }
--- bsc_eth/contracts/imigratablewrappedasset.sol
+++ bsc_eth/contracts/imigratablewrappedasset.sol
@@ -2,7 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-interface IMigratableWrappedAsset 
+/// A collection of all methods on a previous WrappedAsset called by the new version of WrappedAsset during migration.
+/// The migration process does not use any WrappedAsset methods on the previous version that are not in this interface.
+interface IMigratableWrappedAsset
     {
+    /// Only called by the next version of WrappedAsset during migration, this method deducts a maximum amount or its total value (whichever is less) from the given account.
+    /// @param   account   The account from which to remove the wrapped MRX.
+    /// @param   maxAmount The maximum amount to deduct in satoshi.
+    /// @return The amount of MRX actually removed.
     function migrationBurn(address account, uint256 maxAmount) external returns (uint256);
     }
