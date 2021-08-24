2c7abf6585a091cdf51b3a7b24c5a93021641e1b
loma <loma@sqdmc.net>
2021-08-24 06:39:47 -0700

    Raise runs count to 1000000 in solc optimization parameters

--- bsc_eth/src/solidityutils.ts
+++ bsc_eth/src/solidityutils.ts
@@ -12,7 +12,7 @@ const DEPLOYED_DIR  = "solidity_utils_deployments";
 const OPTIMIZER_SETTINGS =
     {
     enabled: true,
-    runs:    2,
+    runs:    1000000,
     details:
         {
         peephole:          true,
